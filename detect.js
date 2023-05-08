c31b9284351b1a3b7942d9e266d2cce91abc40c6
Julien Sellier <juju.sel.mail@gmail.com>
2023-05-08 18:15:16 +0200

    poc

---
+++ detect.js
@@ -0,0 +1,44 @@
+// List URLs that play Rick Astley's Never Gonna Give You Up.
+// Links to these URLs will considered Rick Roll attemps.
+const rickRollUrls = [
+	"https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+	"https://www.youtube.com/watch?v=eBGIQ7ZuuiU",
+	"https://www.youtube.com/watch?v=xm3YgoEiEDc"
+]
+
+// List words that are included in legitmate Rick Roll links.
+// Links that are actually pointing to the video without tricking the user.
+const whiteListWords = [
+	"Rick Astley",
+	"Never Gonna Give You Up",
+]
+
+// Detect Rick Roll URLs on the current page.
+// Detected link are prefixed with a "RICK ROLL WARNING" text.
+rickRollUrls.forEach(url => {
+	// Select <a> elements with href set to a rick roll URL
+	const linkEls = document.querySelectorAll(`a[href='${url}']`)
+
+	// Add prefix warning text to link if Rick Roll attempt
+	let numRickRollAttemps = 0
+	linkEls.forEach(linkEl => {
+		if (isLegitimateLink(linkEl)) return // Exclude legitimate links
+		linkEl.innerText = `RICK ROLL WARNING: ${linkEl.innerText}`
+		numRickRollAttemps++
+	})
+	if (numRickRollAttemps == 0) return
+	notifyRickRollAttempt(numRickRollAttemps)
+})
+
+// Simple utility function to check if a link's inner text includes some of the whitelisted words.
+function isLegitimateLink(linkEl) {
+	whiteListWords.forEach(whiteListWord => {
+		if (linkEl.innerText.includes(whiteListWord)) return true
+	})
+	return false
+}
+
+function notifyRickRollAttempt(numRickRollAttemps) {
+	console.log("RICK ROLL WARNING!!!")
+	console.log(`${numRickRollAttemps} Rick Roll attempt(s) have been detected on this page.`)
+}
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Firefox Extension to detect Rick Roll attemps
+
+This extension will add a warning text before links to Rick Astley's Never Gonna Give You Up.
+
+Todo:
+- [ ] CI/CD
+- [ ] Settings page to add custom whitelist words and rick roll URLs to check
+- [ ] Add button to disable checks on a page
+- [ ] Show alert notification when at least one Rick Roll attempt is detected on the current page
+- [ ] Add manifest icon(s)
+
+Build:
+```bash
+zip -9 -r rick_roll_detector.zip manifest.json detect.js assets/icon_48x48.jpg
+```
